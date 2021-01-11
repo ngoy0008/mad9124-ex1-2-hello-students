87937e7e41ad545c1864a55ce6a545c7cebef3ff
nathanngoyi <71542045+nathanngoyi@users.noreply.github.com>
2021-01-11 14:06:44 -0500

    first commit

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,16 @@
 'use strict'
 
 // 1. Read the JSON file into a variable called students
-
+const students = require('./students.json')
 // 2. Iterate over the students array and print Hello with their full names to the console
 // e.g. Hello Walter Baker
-
+students.forEach(student => {
+    console.log(`Hello ${student.firstName} ${student.lastName}`)
+})
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+let numberOfNames = students.filter(student => {
+    return student.lastName.charAt(0) == 'D'
+}).length
+
+console.log(numberOfNames)
